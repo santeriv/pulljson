38f0a7ae2e09172c6a0b3acfb18ec8bfb60a77a1
Santeri Vesalainen <santeri.vesalainen@gmail.com>
2014-04-18 18:09:07 +0000

    added Aptfile and other updates

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var express = require('express')
 var app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
+  app.set('port', process.env.PORT || 80);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'hbs');
   app.use(express.logger('dev'));
@@ -27,15 +27,17 @@ app.configure(function(){
   app.use(app.router);
   app.use(require('less-middleware')({ src: path.join(__dirname, 'public') }));
   app.use(express.static(path.join(__dirname, 'public')));
+  app.use(require('express-bunyan-logger').errorLogger());
 });
-
+/*
 app.configure('development', function(){
   app.use(express.errorHandler());
 });
-
+*/
 app.get('/', routes.index);
 app.get('/jquery', jquery.fetch);
 app.get('/snapshot', jquery.snapshot);
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
